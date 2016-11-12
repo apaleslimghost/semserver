e5727dbc619fb53bb7e7bc84384a5fee80c8201f
Matt Brennan <mattyb1000@gmail.com>
2016-11-12 12:49:57 +0000

    split registry into separate module

--- index.js
+++ index.js
@@ -3,31 +3,11 @@ const semver = require('semver');
 const merge = require('lodash.merge');
 const bodyParser = require('body-parser');
 const url = require('url');
+const {addToRegistry, resolveRegistry} = require('./registry');
 
 const app = express();
 
 const port = process.env.PORT || 7888;
-
-const registry = {};
-
-const addToRegistry = ({service, version, endpoint}) => {
-	merge(registry, {
-		[service]: {
-			[version]: endpoint,
-		},
-	});
-
-	return Promise.resolve({service, version, endpoint});
-};
-
-const resolveRegistry = ({service, version}) => {
-	if(!registry[service]) throw new Error(`Service ${service} is not in registry`);
-	const versions = Object.keys(registry[service]);
-	const match = semver.maxSatisfying(versions, version);
-	if(!semver.validRange(version)) throw new Error(`${version} is not a valid semver range`);
-	if(!match) throw new Error(`No compatible version for ${service}@${version}. Available versions: ${versions.join(', ')}`);
-	return Promise.resolve(registry[service][match]);
-};
 
 app.post('/:service/:version', bodyParser.urlencoded({extended: false}), (req, res, next) => {
 	const {service, version} = req.params;
