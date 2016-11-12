531acce7f1f4af91adb6580981f1496f92a3ec07
Matt Brennan <mattyb1000@gmail.com>
2016-11-12 13:31:05 +0000

    basic api key auth

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const {addToRegistry, resolveRegistry, getService} = require('./registry');
 const app = express();
 
 const port = process.env.PORT || 7888;
+const auth = (req, res, next) => next(req.headers.authorization !== `Token ${process.env.APIKEY}` && new Error('Unauthorized'));
 
 app.options('/:service', (req, res, next) => getService(req.params.service)
 	.then(service => res.json(service))
 	.catch(next)
 );
 
-app.post('/:service/:version', bodyParser.urlencoded({extended: false}), (req, res, next) => {
+app.post('/:service/:version', auth, bodyParser.urlencoded({extended: false}), (req, res, next) => {
 	const {service, version} = req.params;
 	const {endpoint} = req.body;
 	addToRegistry({service, version, endpoint})
