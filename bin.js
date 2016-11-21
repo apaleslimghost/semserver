acf1a11f463afe48450a4b783a81c566521bb7c6
Matt Brennan <mattyb1000@gmail.com>
2016-11-21 19:16:56 +0000

    rejig bin args and vars

--- bin.js
+++ bin.js
@@ -4,9 +4,12 @@ const {
 	npm_package_name: service,
 	npm_package_version: version,
 	npm_package_semserver_host: host,
-	NOW_URL: endpoint,
 	SEMSERVER_KEY: apiKey,
 } = process.env;
+
+const endpoint = process.argv[2];
+
+console.assert(!!endpoint, 'Usage: semserver endpoint.example.com');
 
 const {post} = require('got');
 const logPromise = require('@quarterto/log-promise');
